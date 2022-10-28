6b3a6e0b8eeaad2ad192730db624672a133c2ccf
Lahcen97 <mohamed.lahcen@epitech.eu>
2022-10-28 14:15:08 +0200

    feature: charts tempalte

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -31,7 +31,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       
-      'axios',
+      'axios'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
