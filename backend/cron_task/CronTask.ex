5b4a730320a22b5accf02408f0c684c62f624eed
= <=>
2022-11-04 10:03:45 +0100

    update image definition'

--- backend/cron_task/CronTask.ex
+++ backend/cron_task/CronTask.ex
@@ -4,25 +4,31 @@ defmodule CronTask do
   # clock out every clocked in user and clock them in back
   # allowing for a better aggregation of user presence time
   # TO DO add a field in database allowing to differenciate a system generated clocks from a user generated clock
-  def daily_clock_manager do
+  def daily_clock_manager_clock_out do
     clocked_in_users = Application.get_clocked_in_users()
-
     Enum.map(clocked_in_users, fn %{"status" => status, "user_id" => user_id, "time" => time} ->
       # clock out the user that is still clocked in
       Application.create_clock(user_id, %{
         "time" => Application.get_unix_current_time()
       })
 
-      Process.sleep(90000)
+      # return user_id to reclock them in
+      user_id
+    end)
+  end
 
+  def daily_clock_manager_reclock_in(user_ids) do
+    clocked_in_users = Application.get_clocked_in_users()
+
+    Enum.map(user_ids, fn user_id ->
       # clock in the user back as it may not have yet finished working
       Application.create_clock(user_id, %{
         "time" => Application.get_unix_current_time()
       })
-
-      Process.sleep(1000)
     end)
   end
 end
 
-CronTask.daily_clock_manager()
+clocked_in_users_ids = CronTask.daily_clock_manager_clock_out()
+Process.sleep(90000)
+CronTask.daily_clock_manager_reclock_in(clocked_in_users_ids)
